04d6bc022d3337a70997ea44d4ffb7bc9c869508
flameking <2711554770@qq.com>
2022-05-18 14:54:57 +0800

    创建数据库实体类：Province、City、County；使用litepal操作数据库，目前项目有关litepal的部分都爆红，原因应该包还未引进；

---
+++ app/src/main/java/com/casflawed/flameking/weather/db/County.java
@@ -0,0 +1,50 @@
+package com.casflawed.flameking.weather.db;
+
+public class County extends LitePalSupport{
+    private Integer id;
+    private String  countyName;
+    private Integer weatherId;
+    private Integer cityId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public Integer getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(Integer weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    @Override
+    public String toString() {
+        return "County{" +
+                "id=" + id +
+                ", countyName='" + countyName + '\'' +
+                ", weatherId=" + weatherId +
+                ", cityId=" + cityId +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/casflawed/flameking/weather/db/Province.java
@@ -0,0 +1,40 @@
+package com.casflawed.flameking.weather.db;
+
+public class Province extends LitePalSupport{
+    private Integer id;
+    private Integer provinceCode;
+    private String provinceName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(Integer provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceCode=" + provinceCode +
+                ", provinceName='" + provinceName + '\'' +
+                '}';
+    }
+}
